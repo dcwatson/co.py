a367fedc21edd5787b2148cea1640ecf3fae529b
Dan Watson <dcwatson@gmail.com>
2020-09-14 14:28:22 -0400

    Work with strings, blacken/isort

--- co.py
+++ co.py
@@ -2,45 +2,47 @@
 
 import argparse
 import datetime
-import sys
 import os
 import re
+import sys
 
 # This could probably be an argument or platform-dependent, but Python likes \n, so we do too.
-DEFAULT_LINE_ENDING = b'\n'
+DEFAULT_LINE_ENDING = "\n"
 
 # Some files have a -*- coding: XXX -*- (or similar) line that must be at the top.
-coding_regex = re.compile(b'coding[:=]\\s*([-\\w.]+)')
+coding_regex = re.compile("coding[:=]\\s*([-\\w.]+)")
+
 
 def make_copyright(args, copy_from=None):
-    years = '%04d-%04d' % (copy_from, args.year) if copy_from and args.update else '%04d' % args.year
-    name = ' %s' % args.name if args.name else ''
-    return '%s Copyright %s%s' % (args.comment, years, name)
+    years = "%04d-%04d" % (copy_from, args.year) if copy_from and args.update else "%04d" % args.year
+    name = " %s" % args.name if args.name else ""
+    return "%s Copyright %s%s" % (args.comment, years, name)
+
 
 def add_copyright(data, args):
     lines = data.splitlines(True)
-    copyright_regex = re.compile(b'^' + re.escape(args.comment.encode('ascii')) + b'\\s*Copyright[^\\d]+(?P<from>\\d{4})', re.I)
+    copyright_regex = re.compile("^" + re.escape(args.comment) + r"\s*Copyright[^\d]+(?P<from>\d{4})", re.I)
     copy_line = None
     copy_from = None
     insert_line = 0
     line_ending = DEFAULT_LINE_ENDING
-    for idx, line in enumerate(lines[:args.check_lines]):
+    for idx, line in enumerate(lines[: args.check_lines]):
         if idx == 0 and line:
-            if line.startswith(b'#!'):
+            if line.startswith("#!"):
                 insert_line += 1
-            if line.endswith(b'\r\n'):
-                line_ending = b'\r\n'
-            elif line.endswith(b'\r'):
-                line_ending = b'\r'
+            if line.endswith("\r\n"):
+                line_ending = "\r\n"
+            elif line.endswith("\r"):
+                line_ending = "\r"
         if idx in (0, 1) and coding_regex.search(line):
             insert_line += 1
         match = copyright_regex.match(line)
         if match:
             copy_line = idx
-            copy_from = int(match.groupdict()['from'])
+            copy_from = int(match.groupdict()["from"])
             break
     # TODO: add encoding argument instead of assuming UTF-8
-    copyright = make_copyright(args, copy_from).encode('utf-8') + line_ending
+    copyright = make_copyright(args, copy_from) + line_ending
     if copy_line is not None:
         lines[copy_line] = copyright
     else:
@@ -50,47 +52,66 @@ def add_copyright(data, args):
     if args.strip:
         for idx in range(len(lines)):
             lines[idx] = lines[idx].rstrip() + DEFAULT_LINE_ENDING
-    return b''.join(lines)
+    return "".join(lines)
+
 
 def fix_file(path, args):
     if not args.quiet:
         sys.stdout.write(path)
-        sys.stdout.write(b'\n')
+        sys.stdout.write("\n")
         sys.stdout.flush()
     if args.print_only:
         return
-    with open(path, 'rb') as fp:
+    with open(path, "r", newline="") as fp:
         new_data = add_copyright(fp.read(), args)
     if args.backup:
-        os.rename(path, path + '.bak')
-    with open(path, 'wb') as fp:
+        os.rename(path, path + ".bak")
+    with open(path, "w") as fp:
         fp.write(new_data)
 
+
 def scan_directories(args):
-    ext = '.%s' % args.extension
+    ext = ".%s" % args.extension
     for path in args.path:
         if path.endswith(ext) and os.path.isfile(path):
             fix_file(os.path.abspath(path), args)
         else:
             for root, dirs, files in os.walk(path):
                 for name in files:
-                    if name.startswith('.') or not name.endswith(ext):
+                    if name.startswith(".") or not name.endswith(ext):
                         continue
                     fix_file(os.path.join(root, name), args)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     current_year = datetime.date.today().year
     parser = argparse.ArgumentParser()
-    parser.add_argument('-y', '--year', default=current_year, type=int, help='copyright year (default: %s)' % current_year)
-    parser.add_argument('-n', '--name', help='copyright holder name')
-    parser.add_argument('-u', '--update', action='store_true', help='updates any existing copyright instead of overwriting')
-    parser.add_argument('-e', '--extension', default='py', help='file extension to scan (default: py)', metavar='ext')
-    parser.add_argument('-c', '--comment', default='#', help='comment characters (default: #)', metavar='chars')
-    parser.add_argument('-s', '--strip', action='store_true', help='strip trailing whitespace and convert line endings to UNIX')
-    parser.add_argument('-b', '--backup', action='store_true', help='keep backup copies of the original files')
-    parser.add_argument('-p', '--print-only', action='store_true', help='prints which files would be modified without changing them')
-    parser.add_argument('-q', '--quiet', action='store_true', help='silence any output')
-    parser.add_argument('--check-lines', default=5, type=int, help='number of lines to scan for existing copyright (default: 5)', metavar='num')
-    parser.add_argument('--no-newline', action='store_true', help='do not insert an extra newline after adding a new copyright line')
-    parser.add_argument('path', nargs='+', help='a file or directory')
+    parser.add_argument(
+        "-y", "--year", default=current_year, type=int, help="copyright year (default: %s)" % current_year
+    )
+    parser.add_argument("-n", "--name", help="copyright holder name")
+    parser.add_argument(
+        "-u", "--update", action="store_true", help="updates any existing copyright instead of overwriting"
+    )
+    parser.add_argument("-e", "--extension", default="py", help="file extension to scan (default: py)", metavar="ext")
+    parser.add_argument("-c", "--comment", default="#", help="comment characters (default: #)", metavar="chars")
+    parser.add_argument(
+        "-s", "--strip", action="store_true", help="strip trailing whitespace and convert line endings to UNIX"
+    )
+    parser.add_argument("-b", "--backup", action="store_true", help="keep backup copies of the original files")
+    parser.add_argument(
+        "-p", "--print-only", action="store_true", help="prints which files would be modified without changing them"
+    )
+    parser.add_argument("-q", "--quiet", action="store_true", help="silence any output")
+    parser.add_argument(
+        "--check-lines",
+        default=5,
+        type=int,
+        help="number of lines to scan for existing copyright (default: 5)",
+        metavar="num",
+    )
+    parser.add_argument(
+        "--no-newline", action="store_true", help="do not insert an extra newline after adding a new copyright line"
+    )
+    parser.add_argument("path", nargs="+", help="a file or directory")
     scan_directories(parser.parse_args())
--- tests.py
+++ tests.py
@@ -1,85 +1,85 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
-import subprocess
 import datetime
-import unittest
-import sys
 import os
+import subprocess
+import sys
+import unittest
 
-class AddCopyrightTests (unittest.TestCase):
 
+class AddCopyrightTests(unittest.TestCase):
     def setUp(self):
-        self.name = 'Dan Watson'
+        self.name = "Dan Watson"
         self.year = datetime.date.today().year
         self.directory = os.path.abspath(os.path.dirname(__file__))
-        self.script = os.path.join(self.directory, 'co.py')
+        self.script = os.path.join(self.directory, "co.py")
 
     def assertCopyright(self, args, original, expected):
-        # Python 3 expects bytes, so make sure our test strings are UTF-8 bytes.
-        original = original.encode('utf-8')
-        expected = expected.encode('utf-8')
         # Create a test file and call the script on it.
-        filename = os.path.join(self.directory, 'output.tmp')
-        with open(filename, 'wb') as fp:
+        filename = os.path.join(self.directory, "output.tmp")
+        with open(filename, "w") as fp:
             fp.write(original)
-        subprocess.call([sys.executable, self.script] + args + ['-q', '-e', 'tmp', filename])
-        with open(filename, 'rb') as fp:
+        subprocess.call([sys.executable, self.script] + args + ["-q", "-e", "tmp", filename])
+        with open(filename, "r", newline="") as fp:
             self.assertEqual(fp.read(), expected)
         os.remove(filename)
         # Test the backup file, if one was created.
-        if os.path.exists(filename + '.bak'):
-            with open(filename + '.bak', 'rb') as fp:
+        if os.path.exists(filename + ".bak"):
+            with open(filename + ".bak", "r", newline="") as fp:
                 self.assertEqual(fp.read(), original)
-            os.remove(filename + '.bak')
+            os.remove(filename + ".bak")
 
     def test_basic(self):
-        original = 'pass'
-        expected = '# Copyright 2020 %s\n\npass' % self.name
-        self.assertCopyright(['-y', '2020', '-n', self.name, '-b'], original, expected)
+        original = "pass"
+        expected = "# Copyright 2020 %s\n\npass" % self.name
+        self.assertCopyright(["-y", "2020", "-n", self.name, "-b"], original, expected)
 
     def test_shebang(self):
-        original = '#!/usr/bin/env python\npass'
-        expected = '#!/usr/bin/env python\n# Copyright %04d %s\n\npass' % (self.year, self.name)
-        self.assertCopyright(['-n', self.name], original, expected)
+        original = "#!/usr/bin/env python\npass"
+        expected = "#!/usr/bin/env python\n# Copyright %04d %s\n\npass" % (self.year, self.name)
+        self.assertCopyright(["-n", self.name], original, expected)
 
     def test_coding(self):
-        original = '#!/usr/bin/env python\n# -*- coding: utf-8 -*-\npass'
-        expected = '#!/usr/bin/env python\n# -*- coding: utf-8 -*-\n# Copyright %04d %s\n\npass' % (self.year, self.name)
-        self.assertCopyright(['-n', self.name], original, expected)
+        original = "#!/usr/bin/env python\n# -*- coding: utf-8 -*-\npass"
+        expected = "#!/usr/bin/env python\n# -*- coding: utf-8 -*-\n# Copyright %04d %s\n\npass" % (
+            self.year,
+            self.name,
+        )
+        self.assertCopyright(["-n", self.name], original, expected)
 
     def test_replace(self):
-        original = '# Copyright 2010 Old Corp\npass'
-        expected = '# Copyright %04d %s\npass' % (self.year, self.name)
-        self.assertCopyright(['-n', self.name], original, expected)
+        original = "# Copyright 2010 Old Corp\npass"
+        expected = "# Copyright %04d %s\npass" % (self.year, self.name)
+        self.assertCopyright(["-n", self.name], original, expected)
 
     def test_update(self):
-        original = '# Copyright 2010 blah blah\r\npass  \r\n'
-        expected = '# Copyright 2010-%04d\r\npass  \r\n' % self.year
-        self.assertCopyright(['-u'], original, expected)
+        original = "# Copyright 2010 blah blah\r\npass  \r\n"
+        expected = "# Copyright 2010-%04d\r\npass  \r\n" % self.year
+        self.assertCopyright(["-u"], original, expected)
 
     def test_keep_endings(self):
-        original = '# Copyright 2010\r\npass  \r\n'
-        expected = '# Copyright %04d\r\npass  \r\n' % self.year
+        original = "# Copyright 2010\r\npass  \r\n"
+        expected = "# Copyright %04d\r\npass  \r\n" % self.year
         self.assertCopyright([], original, expected)
 
     def test_strip(self):
-        original = '# Copyright 2010   \rpass    \r\r'
-        expected = '# Copyright %04d\npass\n\n' % self.year
-        self.assertCopyright(['-s'], original, expected)
+        original = "# Copyright 2010   \rpass    \r\r"
+        expected = "# Copyright %04d\npass\n\n" % self.year
+        self.assertCopyright(["-s"], original, expected)
 
     def test_comment(self):
-        original = 'pass'
-        expected = '// Copyright %04d %s\n\npass' % (self.year, self.name)
-        self.assertCopyright(['-n', self.name, '-c', '//'], original, expected)
+        original = "pass"
+        expected = "// Copyright %04d %s\n\npass" % (self.year, self.name)
+        self.assertCopyright(["-n", self.name, "-c", "//"], original, expected)
 
     def test_no_newline(self):
-        original = 'pass'
-        expected = '# Copyright %04d %s\npass' % (self.year, self.name)
-        self.assertCopyright(['-n', self.name, '--no-newline'], original, expected)
+        original = "pass"
+        expected = "# Copyright %04d %s\npass" % (self.year, self.name)
+        self.assertCopyright(["-n", self.name, "--no-newline"], original, expected)
 
     def test_print_only(self):
-        self.assertCopyright(['-y', '2020', '-n', self.name, '-p'], 'pass', 'pass')
+        self.assertCopyright(["-y", "2020", "-n", self.name, "-p"], "pass", "pass")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
